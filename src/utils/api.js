e1d37a49428f908919785d07e0b5cdb5b192401d
MINHDQHe187308 <minhduongqh0311@gmail.com>
2026-05-23 04:31:10 +0700

    Config api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5193/api',
+    baseURL: 'http://localhost:8090/api',
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
